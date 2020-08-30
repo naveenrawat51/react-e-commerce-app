0dac2fb382286a7f14ad1bb3be87fa3e8327b59b
Naveen Rawat <naveen.rawat@sprint.com>
2020-08-30 11:08:51 +0530

    homepage cards added

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Homepage.styles.scss';
+
+const Homepage = () => {
+    return (
+        <div className="homeppage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Hats
+                        </h1>
+                        <span className="subtitle">
+                            shop now
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Jackets
+                        </h1>
+                        <span className="subtitle">
+                            shop now
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Mens
+                        </h1>
+                        <span className="subtitle">
+                            shop now
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Mens
+                        </h1>
+                        <span className="subtitle">
+                            shop now
+                        </span>
+                    </div>
+                </div>
+
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Mens
+                        </h1>
+                        <span className="subtitle">
+                            shop now
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default Homepage;
