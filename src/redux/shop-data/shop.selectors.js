7dac12abdc45de8175ee2223e9655439666d9262
Naveen Rawat <naveen.rawat@sprint.com>
2020-09-21 18:13:00 +0530

    category routing added

--- src/redux/shop-data/shop.selectors.js
+++ src/redux/shop-data/shop.selectors.js
@@ -1,8 +1,25 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
 
 const selectShopData = (state) => state.shop;
 
 export const getSelectedShopData = createSelector(
   [selectShopData],
   (shop) => shop.shopData
+);
+
+export const selectCollection = memoize((collectionURlParam) =>
+  createSelector([getSelectedShopData], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionURlParam]
+    )
+  )
 );
