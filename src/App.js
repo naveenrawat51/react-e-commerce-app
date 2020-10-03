c679b80d9fc702dcd283040ed6091420931710b9
Naveen Rawat <naveen.rawat@sprint.com>
2020-10-03 21:48:07 +0530

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import { GlobalStyle } from "./global.styles";
 import Homepage from "./pages/homepage/Homepage";
 import { Switch, Route, Redirect } from "react-router-dom";
-import ShopPage from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignInSignUp from "./pages/signup-signin/Signup-signin";
 import { connect } from "react-redux";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { createStructuredSelector } from "reselect";
-import CheckoutPage from "./pages/checkout/checkout";
 import { checkUserSession } from "./redux/user/user.actions";
 
+const ShopPageLazyLoad = lazy( () => import('./pages/shop/Shop'));
+const CheckoutPageLazyLoad = lazy( () => import('./pages/checkout/checkout'));
 class App extends Component {
-  unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { checkUserSession } = this.props;
     checkUserSession();
   }
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
 
   render() {
     return (
@@ -30,8 +26,6 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -39,6 +33,10 @@ class App extends Component {
               this.props.currentUser ? <Redirect to="/" /> : <SignInSignUp />
             }
           />
+          <Suspense fallback={<div>Loading....</div>}>
+          <Route path="/shop" component={ShopPageLazyLoad} />
+          <Route exact path="/checkout" component={CheckoutPageLazyLoad} />
+          </Suspense>
         </Switch>
       </div>
     );
